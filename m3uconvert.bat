85dc4c4414da26c06e8022f9acd5c63e3bdc0259
Extremezhazha <extremezhazha@gmail.com>
2019-03-24 01:50:23 -0700

    Update

--- m3uconvert.bat
+++ m3uconvert.bat
@@ -1,10 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 FOR %%A IN (%*) DO (
-    REM Now your batch file handles %%A instead of %1
-    REM No need to use SHIFT anymore.
 	set inname=%%A
-    echo !inname!
+	echo !inname!
 	set auxstr=M7
 	set outname="!inname:~0,-6!!auxstr!.m3u"
 	pwsh -Command "(gc \"!inname!\" -encoding UTF8) -Replace '.*iTunesMedia\\Music\\', '/storage/external_sd/' -Replace '\\', '/' | Out-File -encoding Utf8NoBom !outname!"
